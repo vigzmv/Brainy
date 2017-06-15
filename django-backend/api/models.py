ee107db08ce43ae8bfe1c8588c5691a0c6211c09
vigzmv <vigzmv@outlook.com>
2017-06-15 11:56:06 +0530

    +flow

--- django-backend/api/models.py
+++ django-backend/api/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     user = models.ForeignKey(User, default=1)
     title = models.CharField(max_length=255)
--- django-backend/api/views.py
+++ django-backend/api/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.views.decorators.csrf import csrf_exempt
 
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.core import serializers
 
@@ -10,8 +10,10 @@ from django.core import serializers
 
 from models import Post
 
+
 def index(request):
     return HttpResponse(content=b'Api Server Running')
+
 
 @csrf_exempt
 def addPost(request):
@@ -21,12 +23,17 @@ def addPost(request):
         img_url = request.POST.get('img_url')
         content = request.POST.get('content')
 
-        post = Post.objects.create(title=title, desc=desc, img_url=img_url, content=content)
+        post = Post.objects.create(title=title,
+                                   desc=desc,
+                                   img_url=img_url,
+                                   content=content
+                                   )
 
         return redirect('http://localhost:3000/post/?post=' + str(post.id))
 
     else:
         return HttpResponseNotFound(content=b'Not Found')
+
 
 def getPost(request):
     post_id = int(request.GET.get('post', ''))
@@ -35,8 +42,9 @@ def getPost(request):
         obj = serializers.serialize('json', post)
         return HttpResponse(obj, content_type='json')
     else:
-        result = {'message' : 'Not Found'}
+        result = {'message': 'Not Found'}
         return JsonResponse(result)
+
 
 def getAllPost(request):
     posts = Post.objects.all()
